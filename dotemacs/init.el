858c3027c563e4022fb7e3a2ad4e0a71d584b412
Neil Bhakta <neil477@gmail.com>
2011-11-28 17:17:15 -0500

    ask before quit

--- dotemacs/init.el
+++ dotemacs/init.el
@@ -225,3 +225,19 @@
 (global-set-key (kbd "C-c x") 'fc-eval-and-replace)
 
 
+
+
+;; prompt before exit
+
+(defun ask-before-closing ()
+  "Ask whether or not to close, and then close if y was pressed"
+  (interactive)
+  (if (y-or-n-p (format "Are you sure you want to exit Emacs? "))
+      (if (< emacs-major-version 22)
+          (save-buffers-kill-terminal)
+        (save-buffers-kill-emacs))
+    (message "Canceled exit")))
+
+(when window-system
+  (global-set-key (kbd "C-x C-c") 'ask-before-closing))
+
