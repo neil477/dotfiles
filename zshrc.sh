607bd35cb52bfe67a4e433dbd326980338e66407
Neil B <neil477@gmail.com>
2012-10-10 22:32:03 -0400

    refactored zsh code

--- zshrc.sh
+++ zshrc.sh
@@ -1,6 +1,16 @@
-#!/bin/bash
+#!/bin/zsh
 
 unamestr=`uname`
+
+export ZSH=$HOME/Dropbox/bin/dotfiles/zsh/oh-my-zsh
+export ZSH_CUSTOM=$HOME/Dropbox/bin/dotfiles/zsh/oh-my-zsh-custom
+export ZSH_THEME="weatherby"
+export DISABLE_AUTO_UPDATE="true"
+export DISABLE_AUTO_TITLE="true"
+
+plugins=(git rails3 rvm ruby osx heroku github brew gem gnu-utils node npm redis-cli)
+
+source $ZSH/oh-my-zsh.sh
 
 if [ "$(uname)" = 'Darwin' ];
   then
