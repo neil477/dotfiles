cd974d53f65a119eed5539e0833afb71a2f16f0a
neil <neill477@gmail.com>
2012-10-11 05:50:09 +0000

    refactored PATH variable

--- zsh/nix/env.sh
+++ zsh/nix/env.sh
@@ -1,14 +1,10 @@
 #editor
 export EDITOR="emacs -nw"
     
-export PATH=/opt/local/bin:/opt/local/sbin:/usr/local/mysql/bin:/usr/texbin:/bin:/sbin:/usr/local/bin:/usr/bin:/usr/sbin:/usr/local/bin/:/usr/local/git/bin:$HOME/.cabal/bin:/usr/share:/usr/man:/usr/games:/usr/local/sbin
-
-# for cabal
-export PATH="$HOME/Library/Haskell/bin:$PATH"
+export PATH=$PATH:/opt/local/bin:/opt/local/sbin:/usr/local/mysql/bin:/usr/texbin:/bin:/sbin:/usr/local/bin:/usr/bin:/usr/sbin:/usr/local/bin/:/usr/local/git/bin:$HOME/.cabal/bin:/usr/share:/usr/man:/usr/games:/usr/local/sbin:$HOME/.rvm/bin:$HOME/Library/Haskell/bin:$GEM_PATH
 
 # rvm stuff
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # Load RVM function
 
 export USERWM=`which xmonad`
 # export TERM="urxvt"
-
